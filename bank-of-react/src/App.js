32067691ef0ef97da05e0a1011473cc67480d2f4
DannieC97 <danielchabi97@gmail.com>
2022-01-19 13:40:03 -0500

    init

---
+++ bank-of-react/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+
+
+function App () {
+  return (
+    <Router>
+    <div className="App">
+      Hello, World!
+    </div>
+    </Router>
+  );
+}
+
+export default App;
